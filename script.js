62f6195c33beb39c371a54835d881d98ec3cd3b4
ErinFleming <flemingerin@hotmail.com>
2020-06-20 21:10:16 -0600

    fourth commit

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const listsContainer = document.querySelector('[data-lists]')
-let lists = ['name', 'todo'];
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+let lists = [{
+  id: 1,
+  name: 'name'
+},{
+  id: 2,
+  name: 'todo'
+}];
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.nodeValue
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+})
+
+function createList(name) {
+  
+}
 
 function render() {
-
   clearElement(listsContainer) 
   lists.forEach(list => {
     const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
-    listElement.innerText = list
+    listElement.innerText = list.name
     listsContainer.appendChild(listElement)
   })
 }
