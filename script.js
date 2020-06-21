3a2755d82616816db78cb31553f71621c48d793a
ErinFleming <flemingerin@hotmail.com>
2020-06-20 21:27:18 -0600

    fifth commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
-let lists = [{
-  id: 1,
-  name: 'name'
-},{
-  id: 2,
-  name: 'todo'
-}];
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+let lists = JSON.parse()
+let lists = [];
 
 newListForm.addEventListener('submit', e => {
   e.preventDefault()
-  const listName = newListInput.nodeValue
+  const listName = newListInput.value
   if (listName == null || listName === '') return
   const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  render()
 })
 
 function createList(name) {
-  
+  return { id: Date.now().toString(), name: name, tasks: [] }
 }
 
 function render() {
